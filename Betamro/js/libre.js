c6003f5c4aee79cb7748f5ee24d36efb096de687
Emanuel <107447125+ender346@users.noreply.github.com>
2023-03-25 15:05:04 -0600

    Paginas de Prueba
    Encontraras 3 paginas con diferente diseño, ya que las tres pagina están creadas usando diferentes códigos, una con solo HTML, otra con HTML y CSS y para finalizar una usando HTML, CSS Y JS usando algunos Frameworks como Bootstrap 5 y Particles.js entres otras mas.

---
+++ Betamro/js/libre.js
@@ -0,0 +1,32 @@
+alert('Hola mundo');
+/* alert("hola como estas");*/
+/* var, let, const 
+ string = "cadena de texto";
+ number = 19;
+ booleano = false;
+ */
+ /*
+  const nombre = "hola";
+  let numero =  29; 
+  numero = 23;
+  alert("numero");
+*/
+/*
+ let numero, numero2, numero3;
+ numero = 2;
+ numero2 = 9;
+ numero = 14;
+
+ alert(numero);
+ alert(numero2);
+ alert(numero3);
+let numero = 6;
+let numero2 = 27;
+
+alert(numero * numero2);
+*/
+/*
+Prompt
+
+let nombre = prompt("hola como estas");
+*/
